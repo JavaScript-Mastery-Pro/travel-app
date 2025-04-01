6357795e92456846e82b673a25d744539f2e28c9
anil-jsmastery <anil@jsmastery.pro>
2025-04-01 21:29:39 +0545

    complete auth page with ui and syncusion button component

--- app/root.tsx
+++ app/root.tsx
@@ -12,15 +12,10 @@ import "./app.css";
 import { registerLicense } from "@syncfusion/ej2-base";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: "icon",
+    href: "/assets/icons/logo.svg",
+    type: "image/svg+xml",
   },
 ];
 
