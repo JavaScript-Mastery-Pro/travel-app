b9520eb084deadef786ad325d67ba52484786aca
anil-jsmastery <anil@jsmastery.pro>
2025-04-14 23:25:54 +0545

    pager component

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -28,12 +28,12 @@ export async function clientLoader() {
     await Promise.all([
       getUser(),
       getUsersAndTripsStats(),
-      getAllTrips(),
+      getAllTrips(4, 0),
       getUserGrowthPerDay(),
       getTripsByTravelStyle(),
     ]);
 
-  const allTrips = trips.map((trip) => {
+  const allTrips = trips.allTrips.map((trip) => {
     const tripData = parseTripData(trip.tripDetail);
     return {
       id: trip.$id,
@@ -115,7 +115,7 @@ const Dashboard = ({ loaderData }: Route.ComponentProps) => {
         <section className="flex flex-col gap-5 mt-2.5">
           <h1 className="text-xl font-semibold text-dark-100">Trips</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-7">
-            {allTrips.slice(0, 4).map((trip) => (
+            {allTrips.map((trip) => (
               <TripCard
                 key={trip.id}
                 id={trip.id}
--- app/routes/trips.tsx
+++ app/routes/trips.tsx
@@ -2,6 +2,9 @@ import { getAllTrips } from "~/appwrite/trips";
 import { Header, TripCard } from "~/components";
 import { parseTripData } from "~/lib/utils";
 import type { Route } from "./+types/trips";
+import { PagerComponent } from "@syncfusion/ej2-react-grids";
+import { useState } from "react";
+import type { LoaderFunctionArgs } from "react-router";
 
 export function meta() {
   return [
@@ -10,16 +13,35 @@ export function meta() {
   ];
 }
 
-export async function loader() {
-  return (await getAllTrips()).map(({ $id, tripDetail, imageUrls }) => ({
-    id: $id,
-    tripData: parseTripData(tripDetail),
-    imageUrls: imageUrls ?? [],
-  }));
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const page = parseInt(url.searchParams.get("page") || "1", 10);
+  const limit = 8;
+  const offset = (page - 1) * limit;
+
+  const { allTrips, total } = await getAllTrips(limit, offset);
+
+  return {
+    trips: allTrips.map(({ $id, tripDetail, imageUrls }) => ({
+      id: $id,
+      ...parseTripData(tripDetail),
+      imageUrls: imageUrls ?? [],
+    })),
+    total,
+  };
 }
 
 const Trips = ({ loaderData }: Route.ComponentProps) => {
-  const trips = loaderData;
+  const trips = loaderData.trips as Trip[] | [];
+  const url = new URL(window.location.href);
+  const initialPage = parseInt(url.searchParams.get("page") || "1", 10);
+  const [currentPage, setCurrentPage] = useState(initialPage);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    window.location.search = `?page=${page}`;
+  };
+
   return (
     <main className="flex flex-col gap-10 pb-20 wrapper">
       <Header
@@ -31,18 +53,24 @@ const Trips = ({ loaderData }: Route.ComponentProps) => {
       <section className="flex flex-col gap-5 mt-2.5">
         <h1 className="p-24-semibold text-dark-100">Manage Created Trips</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-7">
-          {trips.map(({ id, tripData, imageUrls }) => (
+          {trips.map((trip) => (
             <TripCard
-              key={id}
-              id={id}
-              name={tripData?.name ?? ""}
-              imageUrl={imageUrls[0]}
-              location={tripData?.itinerary?.[0]?.location ?? ""}
-              tags={[tripData?.interests ?? "", tripData?.travelStyle ?? ""]}
-              price={tripData?.estimatedPrice ?? ""}
+              key={trip.id}
+              id={trip.id}
+              name={trip.name}
+              imageUrl={trip.imageUrls[0]}
+              location={trip.itinerary?.[0]?.location ?? ""}
+              tags={[trip.interests, trip.travelStyle]}
+              price={trip.estimatedPrice}
             />
           ))}
         </div>
+        <PagerComponent
+          totalRecordsCount={loaderData.total}
+          pageSize={8}
+          currentPage={currentPage}
+          click={(args) => handlePageChange(args.currentPage)}
+        />
       </section>
     </main>
   );
