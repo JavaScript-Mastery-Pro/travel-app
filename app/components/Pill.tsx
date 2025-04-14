3c3cc0f0c81ccfeae75b00a5bad1f85cd5c97f1b
Cienias <cieja0@gmail.com>
2025-04-14 16:39:15 +0200

    refactor: use cn utility

--- app/components/Pill.tsx
+++ app/components/Pill.tsx
@@ -1,10 +1,18 @@
+import { cn } from "~/lib/utils";
+
 const Pill = ({ text, bgColor, textColor }: PillProps) => {
   return (
     <div
-      className={`flex-center px-5 py-2 rounded-[40px] w-fit ${bgColor ? bgColor : "bg-success-50"}`}
+      className={cn(
+        "flex-center px-5 py-2 rounded-[40px] w-fit",
+        bgColor ? bgColor : "bg-success-50"
+      )}
     >
       <h3
-        className={`text-xs font-medium ${textColor ? textColor : "text-success-700"}`}
+        className={cn(
+          "text-xs font-medium",
+          textColor ? textColor : "text-success-700"
+        )}
       >
         {text}
       </h3>
